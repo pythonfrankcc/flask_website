a193e709325248035ff07a7adce8ecb4823d3818
pythonfrankcc <francismurithi475@gmail.com>
2019-09-18 19:44:21 +0300

    forms and validation

--- forms.py
+++ forms.py
@@ -1,19 +1,19 @@
-from flask_wtf import Flaskform#uses this instead of the html creation of the forms
+from flask_wtf import FlaskForm#uses this instead of the html creation of the forms
 from wtforms import StringField#enables us to create strings for usernames and other string fields
 from wtforms import PasswordField,SubmitField,BooleanField
 from wtforms.validators import DataRequired, Length ,Email,EqualTo
 
 class RegistrationForm(FlaskForm):
-	username = StringField('Username',validators=[DataRequired(), Length(min = 2,max = 20)])
-	email = StringField('Email',validators =[DataRequired(),Email])
-	password = PasswordField('Password',validators=[DataRequired()Length(min=6,max=10)])
-	password_confirmation = Password('Password Confirmation',validators = [DataRequired(),EqualTo('password')])
-	submit = SubmitField('Sign Up now')
+	username = StringField("Username",validators=[DataRequired(), Length(min = 2,max = 20)])
+	email = StringField("Email",validators =[DataRequired(),Email()])
+	password = PasswordField("Password",validators=[DataRequired(),Length(min=6,max=10)])
+	password_confirmation = PasswordField("Password Confirmation",validators = [DataRequired(),EqualTo("password")])
+	submit = SubmitField("Sign Up now")
 
 #now lets create a login form
 class LoginForm(FlaskForm):
-	email = StringField('Email',validators =[DataRequired(),Email])#want the users to login using the email instead of username
-	password = PasswordField('Password',validators=[DataRequired()Length(min=6,max=10)])
-	#password_confirmation = Password('Password Confirmation',validators = [DataRequired(),EqualTo('password')]) no longer required as they confirmed the password during registration
-	remember = BooleanField('Remember Me')#allows users to stayed login for sometime after thir browser closes using secure cookie
-	submit = SubmitField('Login')
+	email = StringField("Email",validators =[DataRequired(),Email])#want the users to login using the email instead of username
+	password = PasswordField("Password",validators=[DataRequired(),Length(min=6,max=10)])
+	#password_confirmation = Password("Password Confirmation",validators = [DataRequired(),EqualTo("password")]) no longer required as they confirmed the password during registration
+	remember = BooleanField("Remember Me")#allows users to stayed login for sometime after thir browser closes using secure cookie
+	submit = SubmitField("Login")
--- uav_blog.py
+++ uav_blog.py
@@ -1,43 +1,54 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
 
-app.config['SECRET_KEY']='fc29601ad9a75c5a35e421fd9d546aa4ec3ef1052e'
+app.config["SECRET_KEY"]="fc29601ad9a75c5a35e421fd9d546aa4ec3ef1052e"
 
 posts = [
   {
-   'author':'Dr. Mary',
-   'title':'UAV Design1',
-   'content':'First class to do this course work',
-   'date_posted':'2019-12-13'
+   "author":"Dr. Mary",
+   "title":"UAV Design1",
+   "content":"First class to do this course work",
+   "date_posted":"2019-12-13"
   },
   {
-    'author':'Professor Sameer',
-    'title':'UAV Design2',
-    'content':'First class to do this course work',
-    'date_posted':'2019-12-14'
+    "author":"Professor Sameer",
+    "title":"UAV Design2",
+    "content":"First class to do this course work",
+    "date_posted":"2019-12-14"
   }
 ]
 
-@app.route('/')
-@app.route('/home')
+@app.route("/")
+@app.route("/home")
 def home():
-	return render_template ('home.html',posts = posts)
+	return render_template ("home.html",posts = posts)
 
-@app.route('/about')
+@app.route("/about")
 def about():
-	return render_template('about.html',title = 'About')
+	return render_template("about.html",title = "About")
 
-@app.route('/register')
+#remember to make the page to be able to accept other methods in the page
+@app.route("/register", methods=["GET", "POST"])
 def register():
 	form = RegistrationForm()#creating an instance of the registration form
-	return render_template('register.html',title = 'Register',form = form)
+	if form.validate_on_submit():
+		flash(f"Account for {form.username.data} has been created!", "success")#this is the creation of a flash message
+		return redirect(url_for("home"))#this redirects one back to the homepage on registration
+	return render_template("register.html", title="Register", form=form)
 
-@app.route('/login')
+@app.route("/login",methods=["GET", "POST"])
 def login():
-	form = LoginForm()#creating an instance of the login form
-	return render_template('login.html',title = 'Login',form = form)
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 	app.run(debug = True)

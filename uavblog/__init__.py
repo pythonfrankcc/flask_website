7d07dde8cc18767bc479f142930da4b6927e718e
pythonfrankcc <francismurithi475@gmail.com>
2019-09-20 21:20:10 +0300

    added an account page

--- uavblog/__init__.py
+++ uavblog/__init__.py
@@ -12,7 +12,9 @@ app.config["SECRET_KEY"]="fc29601ad9a75c5a35e421fd9d546aa4ec3ef1052e"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
+login_manager = LoginManager(app)#handles all the user sessions in the background thus works in the models.py
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 #mind the positioning of the importation so as to avoid circular importation
 from uavblog import routes
--- uavblog/models.py
+++ uavblog/models.py
@@ -1,7 +1,11 @@
 from datetime import datetime
-from uavblog import db
+from uavblog import db,login_manager
+from flask_login import UserMixin
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class User(db.Model):
     '''creates the User relational table'''
